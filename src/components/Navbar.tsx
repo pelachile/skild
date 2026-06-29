152b0ea028bc2eacbc4ffacd0bb5c793d5afc1b3
Cory Moore <cory@corymoore.com>
2026-06-28 19:18:38 -0500

    added Clerk auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Show, SignInButton, UserButton } from "@clerk/tanstack-react-start";
 import { Link } from "@tanstack/react-router";
 import { LogIn } from "lucide-react";
 
@@ -12,10 +13,15 @@ export const Navbar = () => (
 			</Link>
 		</div>
 		<div className="actions">
-			<Link to="/sign-in/$" className="btn-primary">
-				<LogIn />
-				Sign In
-			</Link>
+			<Show when="signed-in">
+				<UserButton />
+			</Show>
+			<Show when="signed-out">
+				<Link to="/sign-in/$" className="btn-primary">
+					<LogIn size={16} />
+					Sign In
+				</Link>
+			</Show>
 		</div>
 	</nav>
 );
